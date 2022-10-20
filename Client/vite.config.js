607f2a4793fb129cf42b29e28f3d4641ff4b4933
SayaniR97 <sayani.roy@exavalu.com>
2022-10-20 19:04:24 +0530

    build files added

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -7,6 +7,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
+  base:'/uplusExavalu/',
   plugins: [vue({
     template: {
       compilerOptions: {
